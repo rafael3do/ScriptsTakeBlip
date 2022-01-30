6a43106a197e24e80e621929f46cec9cdeadb5c3
Rafael Figueiredo <rafael3do@hotmail.com>
2022-01-30 17:40:28 -0300

    Update Script.js

--- script_formatar_resposta_do_usuario/Script.js
+++ script_formatar_resposta_do_usuario/Script.js
@@ -1,18 +1,18 @@
 function run(entrada) {
     switch (entrada) {
-        case 1:
+        case '1':
             entrada = '1 - Compra de produto';
             break;
-        case 2:
+        case '2':
             entrada = '2 - Venda de produto';
             break;
-        case 3:
+        case '3':
             entrada = '3 - Renegociar';
             break;
-        case 4:
+        case '4':
             entrada = '4 - Falar com atendente';
             break;
-         default:
+         default://valor padrão caso não entre em nenhum dos casos acima
             entrada='valor invalido'
             break;
     }
