9bb2f47d64123c1279c045d6a6abacdfcb6cbdd0
Rafael Figueiredo <rafael3do@hotmail.com>
2022-05-01 16:25:30 -0300

    Created script identify router

---
+++ script_identifica_de_qual_router_veio/Readme.md
@@ -0,0 +1,11 @@
+Casos de uso: 
+
+1° - Passso
+
+2° - Passo
+
+Teste do fluxo:
+
+
+Teste do script:
+
---
+++ script_identifica_de_qual_router_veio/Script.js
@@ -0,0 +1,12 @@
+function run(entrada) {
+    const sufixobot='@msging.net';
+    switch(entrada){
+        case 'routerten'+sufixobot:
+        return 'botblog';
+        case 'celulabot'+sufixobot:
+        return 'botparceiro';
+        case 'routerevento'+sufixobot:
+        return 'botdoevento';
+    }
+    
+}
