4fd0c99d116989018e5f558dc4cbbd43bf700431
Rafael Figueiredo <rafael3do@hotmail.com>
2022-05-01 16:42:06 -0300

    Update Script.js

--- script_identifica_de_qual_router_veio/Script.js
+++ script_identifica_de_qual_router_veio/Script.js
@@ -1,12 +1,12 @@
 function run(entrada) {
     const sufixobot='@msging.net';
     switch(entrada){
-        case 'routerten'+sufixobot:
-        return 'botblog';
-        case 'celulabot'+sufixobot:
-        return 'botparceiro';
-        case 'routerevento'+sufixobot:
-        return 'botdoevento';
+        case 'iddorouter'+sufixobot: //altere o iddorouter para o id do seu router
+        return 'nomedorouter1';//altere o nomedorouter1 para o nome do seu router
+        case 'iddorouter2'+sufixobot: //não esqueça de alterar aqui tambem.
+        return 'nomedorouter2';//não esqueça de alterar aqui tambem.
+        case 'iddorouter3'+sufixobot://não esqueça de alterar aqui tambem.
+        return 'nomedorouter3';//não esqueça de alterar aqui tambem.
     }
     
 }
