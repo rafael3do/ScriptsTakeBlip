b869d1cab7c5970bf0e67317ae87b58e4268c3d5
Rafael Figueiredo <rafael3do@hotmail.com>
2022-08-07 10:33:22 -0300

    Create script validate input cpf

---
+++ script_otimizado_reconhece_cpf/Readme.md
@@ -0,0 +1,9 @@
+Caso de uso : Reconhecer o cpf do usuario que utiliza o bot sem necessidade de fixar uma formação como orientar o contato a mandar sem ponto(11122233344) e traço ou pedir que mande com ponto e traço(111.222.333-44) ou que mande com espaço. Esse script vai entender qualquer uma dessas entras e não vai utilizar regex o outro ponto é que ele pode ser adequado para funcionar com cpf e cnpj sem precisar de um script para cada caso.
+
+
+
+Teste do fluxo:
+
+Teste do script:
+
+Obs:
---
+++ script_otimizado_reconhece_cpf/Script.js
@@ -0,0 +1,16 @@
+function run(cpf){ 
+    try{   
+    if (typeof cpf !== 'integer'){
+    cpf=cpf.toString();    
+    }
+    if (typeof cpf !== 'string') return false;
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false;
+    cpf = cpf.split('').map(el => +el);
+    const rest = (count) => (cpf.slice(0, count-12)
+        .reduce( (soma, el, index) => (soma + el * (count-index)), 0 )*10) % 11 % 10;
+    return rest(10) === cpf[9] && rest(11) === cpf[10];
+}catch{
+    return "Error";
+}
+}
